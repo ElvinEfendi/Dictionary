9b9b32d509ef3c115ef959097f9279f19a3da866
Elvin Efendiyev <elvin.efendiyev@gmail.com>
2011-10-25 23:09:40 +0500

    dictionary

--- dictionary.rb
+++ dictionary.rb
@@ -1,27 +1,22 @@
 dict = {"car"=>"A car is a vehicle with 4 wheels"} #init hash
 exit=false #exit flag
 
-puts 'Welcome to the self-learning dictionary!' #Welcome user
+puts 'Welcome to the self-learning dictionary!'    #Welcome user
 
 while !exit
   print "Please enter a word: "
-  key = gets.chomp #get word
-  if dict.has_key?(key) #look for word
+  key = gets.chomp.downcase                        #get word then clean and lowercase it
+  if dict.has_key?(key)                            #look for word
     puts dict[key]
-  else #inform user and allow to add new word
-    print "Sorry, not found. Add to Dictionary? (y / n): "
-    add = gets.chomp #user's choice
-    if add == "y"
+  else                                             #inform user and allow to add new word
+    print "Sorry, not found."
+    print " Add to Dictionary? (y / n): "
+    add = gets.chomp.downcase                      #user's choice
+    if add == "y"                                  #add new word
       puts "Please, enter decription for #{key}: "
-      value = gets.chomp #get description for new word
-      dict[key]=value #add (word, desc) pair to the dictionary
-    end #add new word
-  end #look for word
-  #following code part is additional to the assignment :)
-  print "Continue? (y/n): "
-  answ = gets.chomp
-  if answ=="n"
-    exit=true #user want to exit
-  end #exit
-end
+      value = gets.chomp                           #get description for new word
+      dict[key]=value                              #add (word, desc) pair to the dictionary
+    end                                            #add new word
+  end                                              #look for word
+end                                                #end while
   
